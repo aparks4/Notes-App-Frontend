4fcf5b9a458fc531422ac73f98a9fc41ade319c4
aparks4 <arynmparks@gmail.com>
2022-12-12 23:53:15 -0800

    auth context and conditional nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,31 @@
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import AuthContext from '../context/AuthContext';
+
+
 function Nav() {
+    const { user, logoutUser } = useContext(AuthContext);
+
+
     return (
-        <div>Nav</div>
+        <nav>
+            {user ? (
+                <>
+                    <Link to="/home">Home</Link>
+                    <Link to="/notes">My Notes</Link>
+                    <Link to="/notes/new">New Note</Link>
+                </>
+            ) : (
+                <>
+                    <Link to="/home">Home</Link>
+                    <Link to="/login">Login</Link>
+                    <Link to="register">Register</Link>
+                </>
+            )
+
+            }
+            
+        </nav>
     )
 }
 
---
+++ src/context/AuthContext.js
@@ -0,0 +1,101 @@
+import { createContext, useState, useEffect } from 'react';
+import jwt_decode from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
+
+const AuthContext = createContext();
+
+export default AuthContext;
+
+export const AuthProvider = ({ children }) => {
+    const [authTokens, setAuthTokens] = useState(() => 
+        localStorage.getItem("authTokens")
+        ? JSON.parse(localStorage.getItem("authTokens"))
+        : null
+    );
+
+    const [user, setUser] = useState(() => 
+        localStorage.getItem("authTokens")
+        ? jwt_decode(localStorage.getItem("authTokens"))
+        : null
+    );
+
+    const [loading, setLoading] = useState(false);
+
+    const navigate = useNavigate();
+
+    const loginUser = async (username, password) => {
+        const response = await fetch("http://localhost:8000/token/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username,
+                password
+            })
+        });
+        const data = response.json();
+        if (response.status === 200) {
+            setAuthTokens(data);
+            setUser(jwt_decode(data.access));
+            localStorage.setItem("authTokens", JSON.stringify(data));
+            navigate("/home");
+            console.log("Auth: ", user);
+        } else {
+            alert("Something went wrong!")
+        }
+    };
+
+    const registerUser = async (username, password, password2) => {
+        const response = await fetch("http://localhost:8000/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username,
+                password,
+                password2
+            })
+        });
+        if (response.status === 201) {
+            navigate('/login');
+        } else {
+            alert("Something went wrong!")
+        }
+    };
+
+    const logoutUser = () => {
+        setAuthTokens(null);
+        setUser(null);
+        localStorage.removeItem("authTokens");
+        navigate('/');
+    };
+
+    const contextData = [
+        user, 
+        setUser,
+        authTokens,
+        setAuthTokens,
+        registerUser,
+        loginUser,
+        logoutUser
+    ];
+
+    useEffect(() => {
+        setLoading(true);
+        if (authTokens) {
+            setUser(jwt_decode(authTokens.access));
+        }
+        setLoading(false);
+    }, [authTokens]);
+
+    
+    if (loading) return <p>Loading...</p>
+
+    return (
+        <AuthContext.Provider value={contextData}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
